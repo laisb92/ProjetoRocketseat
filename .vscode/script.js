3c9997dceb1839c91b0e1a9be67a74691b6887de
Lais Bastos <laisbastost@hotmail.com>
2023-11-21 21:14:31 -0300

    ed readme

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,5 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle('light')
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./images/avatar-lais-light.png")
+  } else {
+    //se tiver sem light mode, mater original
+    img.setAttribute("src", "./images/avatar-lais.png")
+  }
 }
